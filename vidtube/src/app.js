d23a4696dbd76e0d4ed5baeb82551a9418378667
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-08-02 06:51:33 +0530

    create db connection

--- vidtube/src/app.js
+++ vidtube/src/app.js
@@ -1,8 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CROS_ORIIN,
+        credentials : true
+    })
+)
 
+// commen middleware
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true, limit:"16kb"}));
+app.use(express.static("public"));
 
 
 
