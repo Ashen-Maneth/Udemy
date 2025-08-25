19e40ea3d3fd22932fbcea27fd7e5787746be8fe
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-08-25 06:40:32 +0530

    Add CORS support and restructure server setup
    - Integrated CORS middleware for handling cross-origin requests.
    - Created app.js for Express app configuration and middleware setup.
    - Updated package.json and package-lock.json to include CORS dependency.
    - Removed inline route handling from index.js and moved to app.js.
    - Added utility class ApiResponse for standardized API responses.
    - Added .gitkeep to utils directory for version control.

--- Projmanagement/src/index.js
+++ Projmanagement/src/index.js
@@ -1,19 +1,11 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config();
 
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-})  
-
-app.get("/about", (req, res) => {
-    res.send("About Page");
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
