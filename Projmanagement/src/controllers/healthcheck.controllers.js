d2c9748805667cc8165c0f90741113d697935228
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-09-02 07:01:47 +0530

    Implement asyncHandler utility and refactor health check controller for improved error handling

--- Projmanagement/src/controllers/healthcheck.controllers.js
+++ Projmanagement/src/controllers/healthcheck.controllers.js
@@ -1,15 +1,27 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (rq, res ) => {
+/*
+
+// for this we use the asyncHandler
+
+const healthCheck = async (rq, res , next) => {
     try {
+        const user = await getUserFromDB();
         res
         .status(200)
         .json(new ApiResponse(200, {message: "Server is running"}));
     } catch (error) {
-        // res
-        // .status(500)
-        // .json(new ApiResponse(500, {message: error.message}));
+        next(err);
     }
 }
+*/
+
+const healthCheck = asyncHandler(async (rq, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200, {message: "Server is running"}));
+})
+
 
 export {healthCheck}
