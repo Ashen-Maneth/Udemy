25e259980c9839cfb3e802930a40f51dfe886f2e
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-09-24 06:48:29 +0530

    feat: add express-validator for user registration validation and error handling

---
+++ Projmanagement/src/validators/index.js
@@ -0,0 +1,41 @@
+import {body} from "express-validator";
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is not valid"),
+
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLowercase()
+        .withMessage("Username must be lowercase")
+        .isLength({min: 3, max: 20})
+        .withMessage("Username must be between 3 and 20 characters long")
+        .isString()
+        .withMessage("Username must be a string"),
+        
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({min: 6, max: 20})
+        .withMessage("Password must be between 6 and 20 characters long")
+        .isString()
+        .withMessage("Password must be a string"),
+
+        body("fullname")
+        .optional()
+        .trim()
+        .isString()
+        .withMessage("Fullname must be a string")
+    ]
+
+};
+
+export {userRegisterValidator};
