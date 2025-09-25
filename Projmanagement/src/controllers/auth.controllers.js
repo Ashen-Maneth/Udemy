8cf84ae8dae7c6e87b2b3a7a27f5c49abba19796
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-09-25 06:42:00 +0530

    feat: add cookie-parser middleware and implement login functionality with token generation

--- Projmanagement/src/controllers/auth.controllers.js
+++ Projmanagement/src/controllers/auth.controllers.js
@@ -36,6 +36,7 @@ const registerUser = asyncHandler(async (req, res ) => {
         email,
         password,
         username,
+        role,
         isEmailVerified: false
     });
 
@@ -71,4 +72,49 @@ const registerUser = asyncHandler(async (req, res ) => {
 
 });
 
-export {registerUser}
+const login = asyncHandler(async(req,res) => {
+    const {email, password, username} = req.body
+
+    if(!email) {
+        throw new ApiError(400, "Email is required");
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new ApiError(400,"User Does Not Exists");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid){
+        throw new ApiError(400, "Invalid credentionls")
+    }
+
+    const {accessToken, refreshToken } = await genarateAccessAndRefreshTokens(user._id)
+    
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+    
+    const options = {
+        httpOnly: true,
+        secure : true
+    }
+
+    return res 
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken" , refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser,
+                accessToken,
+                refreshToken
+            },
+            "User Logging Successfully"
+        )
+    )
+})
+    
+export {registerUser ,login}
