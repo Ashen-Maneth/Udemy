add7262aaeacb5bf8390ef040eb072e3fe2b6301
K Ashen Maneth <manethpamuditha23b@gmail.com>
2025-09-26 06:29:43 +0530

    feat: add user login validation with email and password checks

--- Projmanagement/src/validators/index.js
+++ Projmanagement/src/validators/index.js
@@ -38,4 +38,17 @@ const userRegisterValidator = () => {
 
 };
 
-export {userRegisterValidator};
+
+const userLoginValidator = () => {
+    return [
+        body("email")
+        .optional()
+        .isEmail()
+        .withMessage("Email is invalid"),
+        body("password")
+        .notEmpty()
+        .withMessage("Password is Required")
+    ];
+};
+
+export {userRegisterValidator , userLoginValidator};
